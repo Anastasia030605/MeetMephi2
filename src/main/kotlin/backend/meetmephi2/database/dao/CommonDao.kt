733baa062514b6aae87681beb99e6a16374b3840
Anastasia <Anastasiya13277@yandex.ru>
2024-04-26 20:24:11 +0300

    Обновила аннотации, там была проблема с репозиторием, не туда аннотацию шлёпнула

--- src/main/kotlin/backend/meetmephi2/database/dao/CommonDao.kt
+++ src/main/kotlin/backend/meetmephi2/database/dao/CommonDao.kt
@@ -2,10 +2,10 @@ package backend.meetmephi2.database.dao
 
 import backend.meetmephi2.database.entity.AbstractEntity
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.NoRepositoryBean
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
-
-@Repository
+@NoRepositoryBean
 interface CommonDao<T : AbstractEntity> : PagingAndSortingRepository<T, Long>, CrudRepository<T, Long> {
     fun findEntityById(id : Long) : T?
 }
--- src/main/kotlin/backend/meetmephi2/database/dao/UserDao.kt
+++ src/main/kotlin/backend/meetmephi2/database/dao/UserDao.kt
@@ -1,5 +1,7 @@
 package backend.meetmephi2.database.dao
 
 import backend.meetmephi2.database.entity.User
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserDao : CommonDao<User>
